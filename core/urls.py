a36c6434b9d9cbea541aa65d96ccc1555a8836a7
carolchep <carolchepsam@gmail.com>
2021-12-06 13:12:54 +0300

    add to cart feature

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import (products,checkout,HomeView,ItemDetailView,add_to_cart)
+
+app_name='core'
+urlpatterns = [
+    path('', HomeView.as_view(),name='home'),
+    path('checkout/', checkout,name='checkout'),
+    path('products/<slug>/', ItemDetailView.as_view(),name='products'),
+    path('add-to-cart/<slug>/', add_to_cart,name='add-to-cart')
+  
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect
+from .models import Item, OrderItem, Order
+from django.utils import timezone
+from django.views.generic import ListView,DetailView
 
-# Create your views here.
+def products(request):
+    context={
+        'items':Item.objects.all()
+    }
+    return render(request,"products.html",context)
+
+def checkout(request):
+    return render(request,"checkout.html")
+
+
+
+class HomeView(ListView):
+    model=Item
+    template_name="home.html"
+
+class ItemDetailView(DetailView):
+    model=Item
+    template_name="products.html"
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item = OrderItem.objects.create(
+        item=item)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            #messages.info(request, "This item quantity was updated.")
+           # return redirect("core:order-summary")
+        else:
+            ordered_date = timezone.now()
+            order=Order.objects.create(user=request.user,ordered_date=ordered_date)
+            order.items.add(order_item)
+           # messages.info(request, "This item was added to your cart.")
+            #return redirect("core:order-summary")
+            return redirect("core:products",slug=slug)
+
+  #  else:
+       # ordered_date = timezone.now()
+       # order = Order.objects.create(
+           # user=request.user, ordered_date=ordered_date)
+        #order.items.add(order_item)
+       # messages.info(request, "This item was added to your cart.")
+        #return redirect("core:order-summary")
