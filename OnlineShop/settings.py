a19bd9d267d92ab17500f28984621c8f07d1a668
carolchep <carolchepsam@gmail.com>
2021-12-13 08:19:39 +0300

    deployment to heroku settings

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -1,12 +1,15 @@
 import os
 from decouple import config
 #ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
-DEBUG = True
+#DEBUG = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 #ALLOWED_HOSTS = []
 
+DEBUG = False
+ 
+ALLOWED_HOSTS = ['127.0.0.1', 'skinsafi.herokuapp.com']
 
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 #SECRET_KEY = config('SECRET_KEY')
@@ -32,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- OnlineShop/urls.py
+++ OnlineShop/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('core.urls',namespace='core')),
-
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
